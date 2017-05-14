d2f35da19659aeca59c2a11c79373d8be7fea978
Terry <tmoffitt@btinternet.com>
2017-05-14 01:51:06 +0100

    committing again

--- JavaHelloWorld.java
+++ JavaHelloWorld.java
@@ -2,5 +2,6 @@ public class JavaHelloWorld {
 	public static void main (String [] args) {
 		System.out.println("Terry's Hello World");
 		System.out.println("Hello again");
+		System.out.println("Another hello");
 	}
 }
